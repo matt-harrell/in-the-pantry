aeccf63d2454546a0ee603b52dfe8a5f6502bef9
Matt Harrell <matthew.629@hotmail.com>
2023-07-05 19:42:01 -0400

    add netlify function

---
+++ netlify/functions/accessAPIKey.ts
@@ -0,0 +1,21 @@
+import { Handler, HandlerEvent, HandlerContext } from "@netlify/functions";
+
+const handler: Handler = async (event: HandlerEvent, context: HandlerContext) => {
+  
+  try {
+    return{
+        statusCode:200,
+        body:JSON.stringify(process.env["SPOONACULAR_API_KEY"])
+    }
+    
+  } catch (error) {
+    console.log(error);
+    
+    return {
+        statusCode: 404,
+        body: error?.toString()
+      };
+  }
+};
+
+export { handler };
