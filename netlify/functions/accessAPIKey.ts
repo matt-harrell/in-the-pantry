46584629d6067e06155437d7e68a8bddc9ef9116
Matt Harrell <matthew.629@hotmail.com>
2023-07-05 21:49:44 -0400

    fix servelss api function

--- netlify/functions/accessAPIKey.ts
+++ netlify/functions/accessAPIKey.ts
@@ -1,11 +1,13 @@
 import { Handler, HandlerEvent, HandlerContext } from "@netlify/functions";
 
-const handler: Handler = async (event: HandlerEvent, context: HandlerContext) => {
+const handler: Handler = async (event: HandlerEvent, context: HandlerContext) => {  
   
   try {
+    const apiKey = process.env["NG_APP_SPOONACULAR_API_KEY"];
+
     return{
         statusCode:200,
-        body:JSON.stringify(process.env["SPOONACULAR_API_KEY"])
+        body:JSON.stringify({ message: `${apiKey}`})
     }
     
   } catch (error) {
