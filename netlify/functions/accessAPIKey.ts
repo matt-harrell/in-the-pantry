07a41cc1ec0ce02e4256952f2115789d1e4aab7f
Matt Harrell <matthew.629@hotmail.com>
2023-07-09 17:25:55 -0400

    change netlify function

--- netlify/functions/accessAPIKey.ts
+++ netlify/functions/accessAPIKey.ts
@@ -3,7 +3,7 @@ import { Handler, HandlerEvent, HandlerContext } from "@netlify/functions";
 const handler: Handler = async (event: HandlerEvent, context: HandlerContext) => {  
   
   try {
-    const apiKey = process.env["NG_APP_SPOONACULAR_API_KEY"];
+    const apiKey = process.env.NG_APP_SPOONACULAR_API_KEY;
 
     return{
         statusCode:200,
